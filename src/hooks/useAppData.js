9dfcea1e48143c7a2596efbcc254ccd42dccb2cc
Xavier <xaviersavoie@gmail.com>
2022-01-28 15:52:07 -0500

    cleaned up useappdata hook

--- src/hooks/useAppData.js
+++ src/hooks/useAppData.js
@@ -26,64 +26,59 @@ export default function useAppData() {
     return parsedArray;
   };
 
-  // get all light info
-  useEffect(() => {
-    async function fetchLightsApi() {
-      try {
-        const apiResponse = await axios.get(
-          `http://${bridge}/api/${username}/lights/`
-        );
-        const lightsArray = lightsParser(apiResponse.data);
-        setLights(lightsArray.reverse());
-      } catch (error) {
-        console.log(error);
-      }
+  // Fetch all lights data
+  async function fetchLightsApi() {
+    try {
+      const apiResponse = await axios.get(
+        `http://${bridge}/api/${username}/lights/`
+      );
+      const lightsArray = lightsParser(apiResponse.data);
+      setLights(lightsArray.reverse());
+    } catch (error) {
+      console.log(error);
     }
+  }
 
+  // Fetch all groups data
+  async function fetchGroupApi() {
+    try {
+      const apiResponse = await axios.get(
+        `http://${bridge}/api/${username}/groups/`
+      );
+      const groupsArray = Object.keys(apiResponse.data).map((group) => ({
+        ...apiResponse.data[group],
+        id: group,
+        state: apiResponse.data[group].action,
+        action: apiResponse.data[group].state,
+      }));
+      setGroups(groupsArray.reverse());
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // Fetch all scenes data
+  async function fetchScenesApi() {
+    try {
+      const apiResponse = await axios.get(
+        `http://${bridge}/api/${username}/scenes/`
+      );
+      const parsedScenes = Object.keys(apiResponse.data).map((scene) => ({
+        ...apiResponse.data[scene],
+        id: scene,
+      }));
+      setScenes(parsedScenes);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
     fetchLightsApi();
-  }, []);
-
-  // get all groups info
-  useEffect(() => {
-    async function fetchGroupApi() {
-      try {
-        const apiResponse = await axios.get(
-          `http://${bridge}/api/${username}/groups/`
-        );
-        const groupsArray = Object.keys(apiResponse.data).map((group) => ({
-          ...apiResponse.data[group],
-          id: group,
-          state: apiResponse.data[group].action,
-          action: apiResponse.data[group].state,
-        }));
-        setGroups(groupsArray.reverse());
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
     fetchGroupApi();
-  }, []);
-
-  useEffect(() => {
-    async function fetchScenesApi() {
-      try {
-        const apiResponse = await axios.get(
-          `http://${bridge}/api/${username}/scenes/`
-        );
-        const parsedScenes = Object.keys(apiResponse.data).map((scene) => ({
-          ...apiResponse.data[scene],
-          id: scene,
-        }));
-        // console.log(parsedScenes)
-        setScenes(parsedScenes);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
     fetchScenesApi();
   }, []);
+
 
   return {
     lights,
